cb1ee9e8096f513258438acd4c48f3deffb8dcef
Утицких Дмитрий <samprp458804@gmail.com>
2022-05-25 16:05:15 +0400

    someSerialization expanded

--- Logic/IntegrationLogic/ReadNomFrom1C.cs
+++ Logic/IntegrationLogic/ReadNomFrom1C.cs
@@ -9,12 +9,15 @@ using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using Logic.Models.HelperModels;
+using System.Xml.Serialization;
+using Logic.Models;
+using System.IO;
 
 namespace Logic.IntegrationLogic
 {
     public class ReadNomFrom1C
     {
-        public ТаблицаЗначений Read(БромКлиент клиент)
+        public ТаблицаЗначений Read(БромКлиент клиент, string fileName)
         {
             Запрос запросНоменкулатуры = клиент.СоздатьЗапрос(@"
 				ВЫБРАТЬ
@@ -29,121 +32,23 @@ namespace Logic.IntegrationLogic
 					Наименование			
 			");
             ТаблицаЗначений результат = (ТаблицаЗначений)запросНоменкулатуры.Выполнить();
+            CreateSerializedContent(результат, fileName);
             return результат;
         }
-        public void CreateXLSX(ТаблицаЗначений результат, string filepath)
+        public void CreateSerializedContent(ТаблицаЗначений результат, string fileName)
         {
-            using (SpreadsheetDocument spreadsheetDocument =
-            SpreadsheetDocument.Create(filepath+"Nom", SpreadsheetDocumentType.Workbook))
+            List<Models.NomModel> list = new List<Models.NomModel>();
+            foreach (dynamic ctp in результат)
             {
-                // Создаем книгу (в ней хранятся листы)
-                WorkbookPart workbookpart = spreadsheetDocument.AddWorkbookPart();
-                workbookpart.Workbook = new Workbook();
-                // Получаем/создаем хранилище текстов для книги
-                SharedStringTablePart shareStringPart =
-               spreadsheetDocument.WorkbookPart.GetPartsOfType<SharedStringTablePart>().Count() > 0
-                ?
-               spreadsheetDocument.WorkbookPart.GetPartsOfType<SharedStringTablePart>().First()
-                :
-               spreadsheetDocument.WorkbookPart.AddNewPart<SharedStringTablePart>();
-                // Создаем SharedStringTable, если его нет
-                if (shareStringPart.SharedStringTable == null)
-                {
-                    shareStringPart.SharedStringTable = new SharedStringTable();
-                }
-                // Создаем лист в книгу
-                WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
-                worksheetPart.Worksheet = new Worksheet(new SheetData());
-                // Добавляем лист в книгу
-                Sheets sheets =
-               spreadsheetDocument.WorkbookPart.Workbook.AppendChild<Sheets>(new Sheets());
-                Sheet sheet = new Sheet()
-                {
-                    Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
-                    SheetId = 1,
-                    Name = "Лист"
-                };
-                sheets.Append(sheet);
-                uint rowIndex = 1;
-                foreach (dynamic стр in результат)
-                {
-                    InsertCellInWorksheet(new ExcelCellParameters
-                    {
-                        Worksheet = worksheetPart.Worksheet,
-                        ShareStringPart = shareStringPart,
-                        ColumnName = "A",
-                        RowIndex = rowIndex,
-                        Text = '='+'"'+стр.Наименование+'"',
-                        StyleIndex = 1U
-                    });
-                    InsertCellInWorksheet(new ExcelCellParameters
-                    {
-                        Worksheet = worksheetPart.Worksheet,
-                        ShareStringPart = shareStringPart,
-                        ColumnName = "B",
-                        RowIndex = rowIndex,
-                        Text = '=' + '"'+стр.Артикул+'"',
-                        StyleIndex = 1U
-                    });
-                    rowIndex++;
-                }
-                workbookpart.Workbook.Save();
+                list.Add(new NomModel { Name = ctp.Наименование, Article = ctp.Артикул });
             }
-        }
-        private static void InsertCellInWorksheet(ExcelCellParameters cellParameters)
-        {
-            SheetData sheetData = cellParameters.Worksheet.GetFirstChild<SheetData>();
-            // Ищем строку, либо добавляем ее
-            Row row;
-            if (sheetData.Elements<Row>().Where(r => r.RowIndex ==
-           cellParameters.RowIndex).Count() != 0)
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<NomModel>));
+            using (TextWriter textWr = new StreamWriter(fileName))
             {
-                row = sheetData.Elements<Row>().Where(r => r.RowIndex ==
-cellParameters.RowIndex).First();
+                xmlSerializer.Serialize(textWr, list);
+                textWr.Flush();
+                textWr.Close();
             }
-            else
-            {
-                row = new Row() { RowIndex = cellParameters.RowIndex };
-                sheetData.Append(row);
-            }
-            // Ищем нужную ячейку
-            Cell cell;
-            if (row.Elements<Cell>().Where(c => c.CellReference.Value ==
-           cellParameters.CellReference).Count() > 0)
-            {
-                cell = row.Elements<Cell>().Where(c => c.CellReference.Value ==
-               cellParameters.CellReference).First();
-            }
-            else
-            {
-                // Все ячейки должны быть последовательно друг за другом расположены
-                // нужно определить, после какой вставлять
-                Cell refCell = null;
-                foreach (Cell rowCell in row.Elements<Cell>())
-                {
-                    if (string.Compare(rowCell.CellReference.Value,
-                   cellParameters.CellReference, true) > 0)
-                    {
-                        refCell = rowCell;
-                        break;
-                    }
-                }
-                Cell newCell = new Cell()
-                {
-                    CellReference = cellParameters.CellReference
-                };
-                row.InsertBefore(newCell, refCell);
-                cell = newCell;
-            }
-            // вставляем новый текст
-            cellParameters.ShareStringPart.SharedStringTable.AppendChild(new
-           SharedStringItem(new Text(cellParameters.Text)));
-            cellParameters.ShareStringPart.SharedStringTable.Save();
-            cell.CellValue = new
-           CellValue((cellParameters.ShareStringPart.SharedStringTable.Elements<SharedStringItem>().
-           Count() - 1).ToString());
-            cell.DataType = new EnumValue<CellValues>(CellValues.SharedString);
-            cell.StyleIndex = cellParameters.StyleIndex;
         }
     }
 }
